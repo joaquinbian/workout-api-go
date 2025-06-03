7111534cbed6546d7d897b3b7a8c46fd40749961
joaquinbian <joacobian21@gmail.com>
2025-06-03 02:10:18 -0300

    mas

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import "fmt"
 	formatters:
 	- %s -> strings
 	- %d -> digitos (int..)
+	- %f -> float
 	- %t -> booleanos
 */
 
@@ -42,4 +43,47 @@ func main() {
 	}
 
 	fmt.Println(message)
+
+	//zero values (valores por defecto)
+	//previenen nil errors
+	var defaultInt int       //0
+	var defaultFloat float64 //0.0
+	var defaultString string //""
+	var defaultBool bool     //false
+
+	fmt.Printf("Default: %d, %f, %s, %t\n", defaultInt, defaultFloat, defaultString, defaultBool)
+
+	//constantes
+	const pi = 3.14
+
+	const (
+		Lunes     = 1
+		Martes    = 2
+		Miercoles = 3
+	)
+
+	const typedPi float64 = 3.14
+
+	const (
+		Enero = iota //0
+		Feb          //1
+		Mar          //2
+		Abr          //3
+		May          //4
+	)
+
+	fmt.Printf("Meses: %d, %d, %d, %d, %d\n", Enero, Feb, Mar, Abr, May)
+	add(2, 3)
+	s, p := calcSumAndProduct(3, 3)
+
+	fmt.Printf("los resultados de la suma y multiplicacion de 3 y 3 son: suma: %d, mult: %d", s, p)
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+// si devuelve 2 resultados, especificamos todo en parentesis
+func calcSumAndProduct(a, b int) (int, int) {
+	return a + b, a * b
 }
